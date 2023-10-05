2534e86dd18043bc8741f691d304a3c16e7eea94
maccha <chihokoku8060@gmail.com>
2023-10-06 00:26:26 +0900

    delete setText

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useCallback } from "react";
-import {MemoList} from "./MemoList"
-import {useMemoList} from "../hooks/useMemoList";
-import {Title} from "./Title"
+import { MemoList } from "./MemoList"
+import { useMemoList } from "../hooks/useMemoList";
+import { Title } from "./Title"
 import { MemoForm } from "./MemoForm"
  
 const App = () =>  {
   
   const { memos, addTodo, deleteTodo } = useMemoList();
-  // const [memos, setMemos] = useState([]);
   const [text, setText] = useState("");
 
   // テキストボックスの値が変更されたときのハンドラ
@@ -18,7 +17,6 @@ const App = () =>  {
   // メモを追加するハンドラ
   const onClickAdd = useCallback(() => {
     addTodo(text);
-    // クリック時にformが空にならない
     setText("");
   },[text]);
 
@@ -32,7 +30,7 @@ const App = () =>  {
     <div>
       <Title title="簡単メモアプリ"/>
       {/* useStateを使用した関数を渡す際にはuseStateの変数と関数を渡す必要あり */}
-      <MemoForm text={text} setText={setText} onClickAdd={onClickAdd}  onChangeText={onChangeText}/>
+      <MemoForm text={text} onClickAdd={onClickAdd}  onChangeText={onChangeText}/>
       <MemoList memos={memos} onClickDelete={onClickDelete} />
     </div>
   );
